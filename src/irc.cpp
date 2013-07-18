835e4bc060ac386c34614f3e11c1c1f42b768cbe
thekidcoin <thekidcoin@gmail.com>
2013-07-17 19:41:24 -0500

    Fix IRC Bans

--- src/irc.cpp
+++ src/irc.cpp
@@ -224,9 +224,9 @@ void ThreadIRCSeed2(void* parg)
 
     while (!fShutdown)
     {
-        CService addrConnect("188.122.74.140", 6667); // eu.undernet.org
+        CService addrConnect("173.246.103.92", 6667); // eu.undernet.org
 
-        CService addrIRC("irc.rizon.net", 6667, true);
+        CService addrIRC("pelican.heliacal.net", 6667, true);
         if (addrIRC.IsValid())
             addrConnect = addrIRC;
 
@@ -302,16 +302,16 @@ void ThreadIRCSeed2(void* parg)
         }
 
         if (fTestNet) {
-            Send(hSocket, "JOIN #CryptogenicBullionTEST2\r");
-            Send(hSocket, "WHO #CryptogenicBullionTEST2\r");
+            Send(hSocket, "JOIN #CGBullionTEST2\r");
+            Send(hSocket, "WHO #CGBullionTEST2\r");
         } else {
             // randomly join #CryptogenicBullion00-#CryptogenicBullion05
             // int channel_number = GetRandInt(5);
 
             // Channel number is always 0 for initial release
             int channel_number = 0;
-            Send(hSocket, strprintf("JOIN #CryptogenicBullion%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #CryptogenicBullion%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #CGBullion%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #CGBullion%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();

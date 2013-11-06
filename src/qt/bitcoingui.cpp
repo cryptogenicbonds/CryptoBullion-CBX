b2ffde534df4dce5f1b610e8eb2b9212fd8f3474
mercury <mercury@mercury-MS-7751.(none)>
2013-11-06 00:00:00 -0500

    edited Tx fee message

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -623,9 +623,10 @@ void BitcoinGUI::closeEvent(QCloseEvent *event)
 void BitcoinGUI::askFee(qint64 nFeeRequired, bool *payFee)
 {
     QString strMessage =
-        tr("This transaction is over the size limit.  You can still send it for a fee of %1, "
-          "which goes to the nodes that process your transaction and helps to support the network.  "
-          "Do you want to pay the fee?").arg(
+        tr("This transaction is over the size limit.  You can still send it for a fee of %1. "
+          "The fee amount will be destroyed, thus taken out of circulation, which is meant to "
+	  "increase incentive to both miners and stake holders as transaction volume increases "
+	  "over time. Do you want to pay the fee?").arg(
                 BitcoinUnits::formatWithUnit(BitcoinUnits::BTC, nFeeRequired));
     QMessageBox::StandardButton retval = QMessageBox::question(
           this, tr("Confirm transaction fee"), strMessage,

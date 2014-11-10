8d11de000cef13c769ed93a2e3896a26d5331bea
unknown <raizor@gmail.com>
2014-11-10 11:12:55 +0000

    Fix disabled button CSS for toolbar2. Fix build date on debug window. Fix splash image scaling on win32.

--- src/version.cpp
+++ src/version.cpp
@@ -37,11 +37,11 @@ const std::string CLIENT_NAME("Satoshi");
 #endif
 
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
-#define GIT_ARCHIVE 1
-#ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "df9d55a"
-#    define GIT_COMMIT_DATE "$Format:%cD"
-#endif
+//#define GIT_ARCHIVE 0
+//#ifdef GIT_ARCHIVE
+//#    define GIT_COMMIT_ID "df9d55a"
+//#    define GIT_COMMIT_DATE "$Format:%cD"
+//#endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-g" commit

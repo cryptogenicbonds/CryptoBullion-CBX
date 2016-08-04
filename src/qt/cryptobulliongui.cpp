02f7f82309ed3b02238a03a4bdc9cddcd2a99b5b
Alex4J <monkeycoin@protonmail.ch>
2016-08-04 23:12:06 +0200

    Fixing bar size

--- src/qt/cryptobulliongui.cpp
+++ src/qt/cryptobulliongui.cpp
@@ -137,8 +137,8 @@ CryptobullionGUI::CryptobullionGUI(bool fIsTestnet, QWidget *parent):
     // Status bar notification icons
     QFrame *frameBlocks = new QFrame();
     frameBlocks->setContentsMargins(0,0,0,0);
-    frameBlocks->setMinimumWidth(56);
-    frameBlocks->setMaximumWidth(56);
+    frameBlocks->setMinimumWidth(74);
+    frameBlocks->setMaximumWidth(74);
     QHBoxLayout *frameBlocksLayout = new QHBoxLayout(frameBlocks);
     frameBlocksLayout->setContentsMargins(3,0,3,0);
     frameBlocksLayout->setSpacing(3);
